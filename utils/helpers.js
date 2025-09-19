36d21805c0de41174c830bed143eebfc24548156
isholah360 <isholah360@gmail.com>
2025-09-19 02:23:31 -0700

    vercel-change

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-exports.generateReference = () => `txn_${uuidv4()}`;
+// exports.generateReference = () => `txn_${uuidv4()}`;
 
-exports.formatAmount = (amount) => (amount / 100).toFixed(2); // Paystack uses kobo
+// exports.formatAmount = (amount) => (amount / 100).toFixed(2); // Paystack uses kobo
 
-exports.sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// exports.sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
